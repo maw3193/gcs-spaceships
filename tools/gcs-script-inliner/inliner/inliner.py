8c48884e7e9f44262fcabebbf541d804b83af56b
Jonathan Maw <jonathan.maw@gmail.com>
2026-06-07 17:12:28 +0100

    Script mutators work, using pydantic

--- tools/gcs-script-inliner/inliner/inliner.py
+++ tools/gcs-script-inliner/inliner/inliner.py
@@ -4,18 +4,19 @@ Inlining code
 The top-level code for performing inlining
 """
 import json
-from pathlib import Path
 
 from .config import Config
-from .gcs import EquipmentList
+from .gcs import EquipmentList, gcs_library_files
 
 def inline(config: Config) -> None:
     # Inlining takes two big things and munges them together:
     # 1. Collecting all the javascript from config.javascript_sources.
     # 2. Inserting functions (and constants I guess) from javascript sources into gcs.
     # first, let's start small, I have minimal gcs and javascript to prove it works.
-    for library: Path in config.libraries:
-        
+    for file_path in gcs_library_files(config.libraries):
+        equipment = EquipmentList.model_validate_json(file_path.read_text())
+        equipment.mutate_scripts(str.upper)
+        print(equipment)
 
     # Ok, I have enough gcs processing to see how this'll work. I need to parse an manipulate gcs files.
     
