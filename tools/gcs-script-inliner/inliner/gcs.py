ebf32da41376d48130ce9cdad6ca2877f0f5d6d9
Jonathan Maw <jonathan.maw@gmail.com>
2026-06-07 16:17:17 +0100

    WIP: figure out mutating scripts inline, have another headache about json

--- tools/gcs-script-inliner/inliner/gcs.py
+++ tools/gcs-script-inliner/inliner/gcs.py
@@ -4,47 +4,48 @@ GCS
 Code for handling the GCS side of inlining
 """
 
-# AAARGH! THERE'S TOO MUCH TO DESCRIBE THE WHOLE DATA MODEL IN PYTHON
-# USE A TYPEDDICT SO IT'S ROUNDTRIPPABLE WITHOUT DESCRIBING EVERYTHING
-from pydantic import BaseModel, PositiveInt
-from typing import Any, Optional, Self
+import re
+from typing import Any, Optional, Self, TypedDict
+from collections.abc import Callable
 
-class EquipmentModifier(BaseModel):
+type ScriptMutator = Callable[[str], str]
+
+def mutate_scripted_string(s: str, mutator: ScriptMutator) -> str:
+    def mutate_match(match):
+        return mutator(match.group(0))
+    script_expr = r"<script>(.*?)</script>"
+    return re.sub(script_expr, mutate_match, s)
+
+class EquipmentModifier(TypedDict, total=False):
     pass
 
-class Features(BaseModel):
+class PrereqList(TypedDict, total=False):
     pass
 
-class PrereqList(BaseModel):
+class Weapon(TypedDict, total=False):
     pass
 
-class EquipmentItem(BaseModel):
-    id: str
-    description: Optional[str]
-    reference: Optional[str]
-    reference_highlight: Optional[str]
-    local_notes: Optional[str]
-    tech_level: Optional[str]
-    legality_class: Optional[str]
-    tags: Optional[list[str]] = []
-    base_value: Optional[str]
-    base_weight: Optional[str]
-    uses: Optional[int]
-    max_uses: Optional[PositiveInt]
-    prereqs: Optional[PrereqList]
-    weapons: Optional[list[Weapon]] = []
-    features: Optional[Features]
-    ignore_weight_for_skills: bool = False
-    vtt_notes: Optional[str]
-    replacements: dict[str, str] = {}
-    modifiers: list[EquipmentModifier] = []
-    rated_strength: Optional[int]
-    quantity: Optional[int]
-    level: Optional[int]
-    equipped: bool = False
-    third_party: dict[str, Any] = {} # lol I've never seen this used
+class EquipmentItem(TypedDict, total=False):
+    local_notes: Optional[str] # may contain scripts in tags
+    base_value: Optional[str] # is a script or literal
+    base_weight: Optional[str] # is a script or literal
+    prereqs: Optional[PrereqList] # prereqs may contain scripts
+    weapons: Optional[list[Weapon]] # weapons may contain scripts
+    modifiers: list[EquipmentModifier] = [] # modifiers may contain scripts
     children: Optional[list[Self]]
 
-class EquipmentList(BaseModel):
-    version: int
+    def mutate_scripts(self, mutator: ScriptMutator):
+        if self.base_value:
+            self.base_value = mutator(self.base_value)
+        if self.base_weight:
+            self.base_weight = mutator(self.base_weight)
+        if self.local_notes:
+            self.local_notes = mutate_scripted_string(self.local_notes, mutator)
+
+class EquipmentList(TypedDict, total=False):
+    version: int # assert it matches the version I expect
     rows: list[EquipmentItem]
+
+    def mutate_scripts(self, mutator: ScriptMutator):
+        for row in rows:
+            row.mutate_scripts(mutator)
--- tools/gcs-script-inliner/inliner/inliner.py
+++ tools/gcs-script-inliner/inliner/inliner.py
@@ -3,10 +3,19 @@ Inlining code
 
 The top-level code for performing inlining
 """
+import json
+from pathlib import Path
+
 from .config import Config
+from .gcs import EquipmentList
 
 def inline(config: Config) -> None:
     # Inlining takes two big things and munges them together:
     # 1. Collecting all the javascript from config.javascript_sources.
     # 2. Inserting functions (and constants I guess) from javascript sources into gcs.
     # first, let's start small, I have minimal gcs and javascript to prove it works.
+    for library: Path in config.libraries:
+        
+
+    # Ok, I have enough gcs processing to see how this'll work. I need to parse an manipulate gcs files.
+    
