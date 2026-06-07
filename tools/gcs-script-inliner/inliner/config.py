e9e4b92868926aa413af2a55f13e9058c38e132b
Jonathan Maw <jonathan.maw@gmail.com>
2026-06-07 17:43:05 +0100

    Run gcs convert after mangling

--- tools/gcs-script-inliner/inliner/config.py
+++ tools/gcs-script-inliner/inliner/config.py
@@ -6,6 +6,7 @@ Types and code for configuring GCS Script Inliner
 from pathlib import Path
 from pydantic import BaseModel, Field, FilePath, DirectoryPath
 from pydantic_yaml import parse_yaml_file_as
+from typing import Optional
 
 class Config(BaseModel):
     """
@@ -15,6 +16,7 @@ class Config(BaseModel):
         javascript_sources: A list of javascript file paths, or paths to directories that contain javascript files
         libraries: A list of paths to directories that contain gcs files that should have javascript inlined
     """
+    gcs_command: Optional[str] = "gcs"
     javascript_sources: list[FilePath | DirectoryPath] = Field(min_length=1)
     libraries: list[FilePath | DirectoryPath] = Field(min_length=1)
 
--- tools/gcs-script-inliner/inliner/inliner.py
+++ tools/gcs-script-inliner/inliner/inliner.py
@@ -4,6 +4,7 @@ Inlining code
 The top-level code for performing inlining
 """
 import json
+import subprocess
 
 from .config import Config
 from .gcs import EquipmentList, gcs_library_files
@@ -13,10 +14,13 @@ def inline(config: Config) -> None:
     # 1. Collecting all the javascript from config.javascript_sources.
     # 2. Inserting functions (and constants I guess) from javascript sources into gcs.
     # first, let's start small, I have minimal gcs and javascript to prove it works.
-    for file_path in gcs_library_files(config.libraries):
+    files = list(gcs_library_files(config.libraries))
+    for file_path in files:
         equipment = EquipmentList.model_validate_json(file_path.read_text())
         equipment.mutate_scripts(str.upper)
         file_path.write_text(equipment.model_dump_json())
 
-    # Ok, I have enough gcs processing to see how this'll work. I need to parse an manipulate gcs files.
+    cmdline = [config.gcs_command, "-convert"]
+    cmdline.extend(f.as_posix() for f in files)
+    subprocess.run(cmdline)
     
